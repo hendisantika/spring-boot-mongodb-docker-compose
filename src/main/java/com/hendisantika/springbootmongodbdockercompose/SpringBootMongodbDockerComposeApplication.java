009a02883390267ad1cfd466936cb5befb6f6648
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-26 08:29:53 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmongodbdockercompose/SpringBootMongodbDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmongodbdockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbDockerComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMongodbDockerComposeApplication.class, args);
+    }
+
+}
