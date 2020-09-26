3433f6ab7bbcd27bd78a0780a4bd100920913717
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-26 08:39:41 +0700

    #9 - Add addFruit & getAllFruit API in Fruit Controller

--- src/main/java/com/hendisantika/springbootmongodbdockercompose/controller/FruitController.java
+++ src/main/java/com/hendisantika/springbootmongodbdockercompose/controller/FruitController.java
@@ -1,8 +1,17 @@
 package com.hendisantika.springbootmongodbdockercompose.controller;
 
+import com.hendisantika.springbootmongodbdockercompose.dto.FruitRequest;
+import com.hendisantika.springbootmongodbdockercompose.model.Fruit;
 import com.hendisantika.springbootmongodbdockercompose.service.FruitService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,6 +30,18 @@ public class FruitController {
 
     public FruitController(FruitService fruitService) {
         this.fruitService = fruitService;
+    }
+
+    @PostMapping("/fruits")
+    public ResponseEntity addFruit(@RequestBody FruitRequest fruitRequest) {
+        log.info("Request : {}", fruitRequest);
+        fruitService.saveFruit(fruitRequest.toFruitModel());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/fruits")
+    public List<Fruit> getAllFruit() {
+        return fruitService.findAll();
     }
 
 }
